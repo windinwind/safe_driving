2db949463816cd3847373478b5cf9c8944d94aa4
wusling <helenzhang250@gmail.com>
2017-10-11 17:11:49 -0700

    login & register done. needs server verification

--- safeDrivingApp/app/src/main/java/lazydroid/safedriving/MainActivity.java
+++ safeDrivingApp/app/src/main/java/lazydroid/safedriving/MainActivity.java
@@ -1,5 +1,6 @@
 package lazydroid.safedriving;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,67 +18,131 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import android.os.AsyncTask;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText userNameEdit;
-    EditText passwordEdit;
+    public static String loginURL = "http://34.210.113.123/login";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userNameEdit = (EditText) findViewById(R.id.user_name);
-        passwordEdit = (EditText) findViewById(R.id.password);
-
     }
 
     protected void loginButtonClicked(View v) {
         //System.out.println("button clicked");
+        EditText userNameEdit = (EditText) findViewById(R.id.user_name);
+        EditText passwordEdit = (EditText) findViewById(R.id.password);
         String username = userNameEdit.getText().toString();
         String password = passwordEdit.getText().toString();
 
         Log.d("username = ", username);
         Log.d("password = ", password);
-
         new NetworkConnection().execute(username, password);
+        //sendLoginInfo(username, password);
 
     }
-}
-    class NetworkConnection extends AsyncTask<String, Object, Void> {
+
+    protected void sendLoginInfo(String username, String password) {
+        //do network connection
+        try {
+            URL url = new URL(loginURL);
+            //establish connection with the server login page
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            //set user name and password into the get request
+            Log.d("before encode name", username);
+            Log.d("before encode password", password);
+            String encoded = Base64.encodeToString((username + ":" + password).getBytes("UTF-8"), Base64.NO_WRAP);
+            Log.d("encoded String", encoded);
+            urlConnection.setRequestProperty("Authorization", "Basic " + encoded);
+            urlConnection.setRequestMethod("GET");
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            //check the response from the server
+            String login_status = rd.readLine();
+            Log.d("login status is ", login_status);
+            String login_status_pair[] = login_status.split(":");
+
+            rd.close();
+            urlConnection.disconnect();
+
+            //TODO: if success, jump to start SD page
+            //TODO: if fails, print response message
+            if (login_status_pair.length != 2) {
+                TextView feedback = (TextView) findViewById(R.id.feedback_box);
+                feedback.setText("server error");
+                return;
+            }
+
+            if (login_status_pair[1].equals("success")) {
+
+            } else {
+                TextView feedback = (TextView) findViewById(R.id.feedback_box);
+                feedback.setText("Invalid username or password");
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    class NetworkConnection extends AsyncTask<String, Integer, Void> {
+
 
         @Override
         protected Void doInBackground(String... params) {
-            if(params.length != 2) {
-                Log.d( "doInBackground: ", "argument number wrong");
+            if (params.length != 2) {
+                Log.d("doInBackground: ", "argument number wrong");
                 return null;
             }
             String username = params[0];
             String password = params[1];
-            //do network connection in async task
+
+            //do network connection
             try {
-                URL url = new URL("http://192.168.31.147/login");
+                URL url = new URL(loginURL);
                 //establish connection with the server login page
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
                 //set user name and password into the get request
                 Log.d("before encode name", username);
                 Log.d("before encode password", password);
-                String encoded = Base64.encodeToString((username + ":" + password).getBytes("UTF-8"),Base64.NO_WRAP);
+                String encoded = Base64.encodeToString((username + ":" + password).getBytes("UTF-8"), Base64.NO_WRAP);
                 Log.d("encoded String", encoded);
-                urlConnection.setRequestProperty("Authorization", "Basic "+encoded);
+                urlConnection.setRequestProperty("Authorization", "Basic " + encoded);
                 urlConnection.setRequestMethod("GET");
 
                 BufferedReader rd = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
                 //check the response from the server
                 String login_status = rd.readLine();
                 Log.d("login status is ", login_status);
-                //TODO: if success, jump to start SD page
-                //TODO: if fails, print response message
+                String login_status_pair[] = login_status.split(":");
 
                 rd.close();
                 urlConnection.disconnect();
+
+                //TODO: if success, jump to start SD page
+                //TODO: if fails, print response message
+                if (login_status_pair.length != 2) {
+                    //TextView feedback = (TextView)findViewById(R.id.feedback_box);
+                    //feedback.setText("server error");
+                    return null;
+                }
+
+                if (login_status_pair[1].equals("success")) {
+                    publishProgress(1);
+                } else {
+                    //TextView feedback = (TextView)findViewById(R.id.feedback_box);
+                    //feedback.setText("Invalid username or password");
+                    publishProgress(0);
+                }
+
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
@@ -86,4 +151,27 @@ public class MainActivity extends AppCompatActivity {
 
             return null;
         }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            if (progress.equals(0)) {
+                TextView feedback = (TextView) findViewById(R.id.feedback_box);
+                feedback.setText("Invalid username or password");
+            } else {
+                TextView feedback = (TextView) findViewById(R.id.feedback_box);
+                feedback.setText("Login success");
+
+                Intent intent = new Intent(MainActivity.this, SafeDrivingActivity.class);
+                intent.putExtra("login", "success");
+                startActivity(intent);
+
+            }
+        }
+
+    }
+
+    protected void registerButtonClicked(View v){
+        Intent intent = new Intent(this, UserRegisterActivity.class);
+        startActivity(intent);
+    }
 }
